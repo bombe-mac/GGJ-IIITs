2582861d8217b34593cebe80ce0ceaa89ca3165c
Aditya12313 <deshmukh10.aditya@gmail.com>
2026-01-17 11:43:21 +0530

    Sponsor bg change

--- src/app/sponsor/page.tsx
+++ src/app/sponsor/page.tsx
@@ -4,8 +4,21 @@ import Image from 'next/image';
 
 export default function PeoplePage() {
   return (
-    <div className="w-screen min-h-screen bg-black flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-10 px-6">
+   <div className="relative w-screen min-h-screen bg-black flex items-center justify-center overflow-hidden">
+  
+  {/* Moving blue glow (upper half only) */}
+  <div
+    className="absolute top-0 left-0 w-full h-1/2 pointer-events-none animate-gradientMove"
+    style={{
+      background:
+        'radial-gradient(circle at 20% 30%, rgba(59,31,209,0.8), rgba(42,13,187,0.6), transparent 70%)',
+      filter: 'blur(80px)',
+    }}
+  />
+
+  {/* Content */}
+  <div className="relative z-10">
+    {<div className="flex flex-col items-center justify-center gap-10 px-6">
 
         {/* Heading */}
         <h1 className="text-white text-center text-xl md:text-4xl font-light tracking-wide max-w-3xl">
@@ -48,7 +61,11 @@ export default function PeoplePage() {
           </a>
 
         </div>
-      </div>
-    </div>
+      </div>}
+  </div>
+</div>
+
+      
+
   );
 }
